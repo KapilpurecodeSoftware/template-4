e8224d50441c488d8dbf5ec1b95174e0ca6d833d
KapilChoudhary88 <choudharykapil8899@gmail.com>
2023-08-18 17:06:04 +0530

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import SecondContainer from './components/secondContainer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SecondContainer />
     </div>
   );
 }
--- src/components/secondContainer.jsx
+++ src/components/secondContainer.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 export default function SecondContainer() {
   return (
     <div>
-      
+      <div  className="text-left">
+          <h2 className="h2-Heading">
+            {" "}
+            Powering The Future Of <br/><span className="work">Work</span>
+            
+          </h2>
+          <div className="loremPara1">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+             elit. Sapiente
+            excepturi dolorem quos. Lorem ipsum dolor sit amet consectetur adipisicing
+             elit. Sapiente
+            excepturi dolorem quos.
+          </div>
+         
+        </div>
     </div>
   )
 }
